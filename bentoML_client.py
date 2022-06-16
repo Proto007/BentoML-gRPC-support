ec33cc41c4872c2073c8bcb7d79f348973a25d69
Sadab Hafiz <sadabhafizny@gmail.com>
2022-06-16 17:34:53 -0400

    Updated testing client code

--- bentoML_client.py
+++ bentoML_client.py
@@ -1,6 +1,8 @@
 # Import necessary modules
 import grpc
-from google.protobuf.json_format import MessageToJson
+from google.protobuf.json_format import MessageToDict
+import numpy as np
+import json
 
 # Import generated files
 import bentoML_pb2
@@ -10,17 +12,23 @@ import bentoML_pb2_grpc
 channel=grpc.insecure_channel('localhost:8000')
 stub=bentoML_pb2_grpc.BentoMLStub(channel)
 
+"""
+    Testing String
+"""
 # Test the Api with a query description. 
 # query_string is a PredictTopicRequest protobuf object 
-description="Nora Stephens’ life is books—she’s read them all—and she is not that type of heroine. Not the plucky one, not the laidback dream girl, and especially not the sweetheart. In fact, the only people Nora is a heroine for are her clients, for whom she lands enormous deals as a cutthroat literary agent, and her beloved little sister Libby. Which is why she agrees to go to Sunshine Falls, North Carolina for the month of August when Libby begs her for a sisters’ trip away—with visions of a small-town transformation for Nora, who she’s convinced needs to become the heroine in her own story. But instead of picnics in meadows, or run-ins with a handsome country doctor or bulging-forearmed bartender, Nora keeps bumping into Charlie Lastra, a bookish brooding editor from back in the city. It would be a meet-cute if not for the fact that they’ve met many times and it’s never been cute. If Nora knows she’s not an ideal heroine, Charlie knows he’s nobody’s hero, but as they are thrown together again and again—in a series of coincidences no editor worth their salt would allow—what they discover might just unravel the carefully crafted stories they’ve written about themselves."
+# description="Nora Stephens’ life is books—she’s read them all—and she is not that type of heroine. Not the plucky one, not the laidback dream girl, and especially not the sweetheart. In fact, the only people Nora is a heroine for are her clients, for whom she lands enormous deals as a cutthroat literary agent, and her beloved little sister Libby. Which is why she agrees to go to Sunshine Falls, North Carolina for the month of August when Libby begs her for a sisters’ trip away—with visions of a small-town transformation for Nora, who she’s convinced needs to become the heroine in her own story. But instead of picnics in meadows, or run-ins with a handsome country doctor or bulging-forearmed bartender, Nora keeps bumping into Charlie Lastra, a bookish brooding editor from back in the city. It would be a meet-cute if not for the fact that they’ve met many times and it’s never been cute. If Nora knows she’s not an ideal heroine, Charlie knows he’s nobody’s hero, but as they are thrown together again and again—in a series of coincidences no editor worth their salt would allow—what they discover might just unravel the carefully crafted stories they’ve written about themselves."
 
-query_string=bentoML_pb2.Input(text_Input=description)
+# query_string=bentoML_pb2.Input(text_Input=description)
+
+"""
+    Testing Numpy
+"""
+a="Sadab"
+arr=np.array([[[1,2],a],[[2,3],"b"],[[3,4],"c"]],dtype=object)
+data=bentoML_pb2.Input(numpyArr_Input=json.dumps(arr.tolist()))
 
 # Call the predict function
-response=stub.api_func(query_string)
+response=stub.api_func(data)
 
-# Change the response to a JSON format
-json_obj=MessageToJson(response)
-
-# Print the output
-print(json_obj)
+print(response.numpyArr_Output)
