eec07da1fda1c8bdf683324b06f3132f2a90fe41
Sadab Hafiz <sadabhafizny@gmail.com>
2022-06-17 15:29:51 -0400

    Using bytesIO to store numpy arrays as bytes

--- start_grpc_service.py
+++ start_grpc_service.py
@@ -2,7 +2,7 @@ import bentoml
 import os
 import sys
 import json
-
+from bentoml.io import NumpyNdarray,Text
 import numpy as np
 
 """
@@ -21,14 +21,14 @@ except:
     Functions to generate protobuf content for specific datatypes
 """
 def get_proto_text(input_type):
-    return f'string text_{input_type} = 1;', f'string text_{input_type}'
+    return f'string text_{input_type} = 1;', f'text_{input_type}'
 
 def get_proto_numpyArr(input_type):
-    return f'string numpyArr_{input_type} = 1;', f"numpyArr_{input_type}"
+    return f'bytes numpyArr_{input_type} = 1;', f"numpyArr_{input_type}"
 
 func_dict={
-    "Text()": get_proto_text,
-    "NumpyNdarray()": get_proto_numpyArr
+    Text: get_proto_text,
+    NumpyNdarray: get_proto_numpyArr
 }
 
 
@@ -37,16 +37,16 @@ def generate_protobuf(input_type,output_type):
     Generates protobuf based on given input_type, output_type and function name
     """
     # Verify input_type and output_type are correct
-    if(input_type not in func_dict) or (output_type not in func_dict):
-        if(input_type not in func_dict):
-            print(f"${input_type} is not a valid type")
-        if(output_type not in func_dict):
-            print(f"${output_type} is not a valid type")
+    if(type(input_type) not in func_dict) or (type(output_type) not in func_dict):
+        if(type(input_type) not in func_dict):
+            print("Input is not a valid type")
+        if(type(output_type) not in func_dict):
+            print("Output is not a valid type")
         raise ValueError("Invalid input types.")
 
     # Map the input and output to the appropriate functions
-    input_proto, input_name=func_dict[input_type]("Input")
-    output_proto, output_name=func_dict[output_type]("Output")
+    input_proto, input_name=func_dict[type(input_type)]("Input")
+    output_proto, output_name=func_dict[type(output_type)]("Output")
 
     #Create the protobuf file
     with open('./protos/bentoML.proto','w') as f:
@@ -67,7 +67,7 @@ def generate_protobuf(input_type,output_type):
 
 for name,api in svc.apis.items():    
     # Create a server and add the lda_model to the server
-    i,o=generate_protobuf(api.input._init_str,api.output._init_str)
+    i,o=generate_protobuf(api.input,api.output)
     svc_func=api.func
 
 def create_server(server_name:str):
@@ -83,6 +83,7 @@ import numpy as np
 import json
 import bentoml
 from concurrent import futures
+from io import BytesIO
 
 svc=bentoml.load("{sys.argv[1]}")
 
@@ -90,14 +91,26 @@ for name,api in svc.apis.items():
     # Create a server and add the lda_model to the server
     svc_func=api.func
 
+def array_to_bytes(arr):
+    to_bytes=BytesIO()
+    np.save(to_bytes,arr,allow_pickle=True)
+    return to_bytes.getvalue()
+
+def bytes_to_array(bytes_arr):
+    load_bytes=BytesIO(bytes_arr)
+    loaded_arr=np.load(load_bytes,allow_pickle=True)
+    return loaded_arr
+
 class BentoMLServicer(bentoML_pb2_grpc.BentoMLServicer):
     def api_func(self, request, context):
-        input_arr=request.{i}
-        input_arr=eval(f"np.array({{input_arr}})")
-        out=svc_func(input_arr)
-        out_json=json.dumps(out.tolist())
-        return bentoML_pb2.Output({o}=out_json)
-        
+        input= request.{i}
+        if "numpyArr" in "{i}":
+            input=bytes_to_array(input)
+        output=svc_func(input)
+        if "numpyArr" in "{o}":
+            output=array_to_bytes(output)
+        return bentoML_pb2.Output({o}=output)
+
 # Create a server and add the lda_model to the server
 server=grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 bentoML_pb2_grpc.add_BentoMLServicer_to_server(BentoMLServicer(),server)
@@ -115,10 +128,3 @@ server.wait_for_termination()
 os.system("python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. protos/bentoML.proto")
 create_server("numpy_server.py")
 os.system("python numpy_server.py")   
-"""
- Potential Libraries (numproto: depracated/not popular, numpy-protobuf(not finished))
- Using repeated Any, doesn't work with error "must be instance of same class"
- Using bytesIO
- Json Dump
-"""
-# dtype is provided and shape is provided
