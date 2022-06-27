5c206531cbe4853373abbee5f255c2af8dfd362f
Sadab Hafiz <sadabhafizny@gmail.com>
2022-06-27 17:53:37 -0400

    Added function to convert from array to proto

--- grpc_service_util.py
+++ grpc_service_util.py
@@ -31,3 +31,53 @@ def is_supported(datatype):
             found_dtype=supported_datatypes[key]
             break
     return found_dtype
+
+# TODO: Add support for datetime and timedelta
+def create_tuple_proto(tuple):
+    if len(tuple)==0:
+        raise ValueError("Provided tuple is either empty or invalid.")
+    tuple_arr=[]
+    for item in tuple:
+        dtype=is_supported(type(item))
+        if(dtype and dtype!="array_"):
+            tuple_arr.append(io_descriptors_pb2.Value(**{f"{dtype}":item}))
+        elif(dtype=="array_"):
+            if(not all(isinstance(x,type(item[0]))for x in item)):
+                val=create_tuple_proto(item)
+                tuple_arr.append(io_descriptors_pb2.Value(tuple_=val))
+            else:
+                val=arr_to_proto(item)
+                tuple_arr.append(io_descriptors_pb2.Value(array_=val))
+        else:
+            raise ValueError(f'Invalid datatype "{type(item).__name__}" within tuple.')
+        
+    return io_descriptors_pb2.Tuple(value_=tuple_arr)
+
+def arr_to_proto(arr):
+    if len(arr)==0:
+        raise ValueError("Provided array is either empty or invalid.")
+    if(not all(isinstance(x,type(arr[0]))for x in arr)):
+        raise ValueError("Entered tuple, expecting array.")
+    
+    dtype=is_supported(type(arr[0]))
+    if(dtype and dtype!="array_"):
+        return io_descriptors_pb2.Array(**{"dtype":dtype,f"{dtype}":arr})
+
+    return_arr=[]
+    is_tuple=False
+    for i in range(len(arr)):
+        if(not all(isinstance(x,type(arr[i][0]))for x in arr[i])):
+            is_tuple=True
+            val=create_tuple_proto(arr[i])
+        else:
+            val=arr_to_proto(arr[i])
+        return_arr.append(val)
+    try:
+        if(is_tuple):
+            return_arr=io_descriptors_pb2.Array(dtype="tuple_",tuple_=return_arr)
+        else:
+            return_arr=io_descriptors_pb2.Array(dtype="array_",array_=return_arr)
+    except:
+        raise ValueError("Entered invalid array of inconsistent shape.")
+    
+    return return_arr
