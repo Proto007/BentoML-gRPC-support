d924bc3020e4f88168f4ae80a95d6903de9e3292
Sadab Hafiz <sadabhafizny@gmail.com>
2022-06-28 13:50:33 -0400

    Changed names in supported_datatypes dictionary

--- grpc_service_util.py
+++ grpc_service_util.py
@@ -3,18 +3,20 @@ import numpy as np
 import io_descriptors_pb2
 from google.protobuf.timestamp_pb2 import Timestamp
 from google.protobuf.duration_pb2 import Duration
+import datetime
 
 supported_datatypes={
-    np.dtype('i4'): "i4",
-    np.dtype('i8'): "i8",
-    np.dtype('u4'): "u4",
-    np.dtype('u8'): "u8",
-    np.dtype('f4'): "f4",
-    np.dtype('f8'): "f8",
+    np.dtype('i4'): "sint32_",
+    np.dtype('i8'): "sint64_",
+    np.dtype('u4'): "uint32_",
+    np.dtype('u8'): "uint64_",
+    np.dtype('f4'): "float_",
+    np.dtype('f8'): "double_",
     np.dtype('?'): "bool_",
     np.unicode_ : "string_",
     np.string_: "bytes_",
-    np.datetime64: "timestamp_",
+    datetime.datetime: "timestamp_",
+    datetime.date:"timestamp_",
     np.timedelta64: "duration_",
     np.ndarray: "array_",
     list: "array_",
@@ -39,9 +41,19 @@ def create_tuple_proto(tuple):
     tuple_arr=[]
     for item in tuple:
         dtype=is_supported(type(item))
-        if(dtype and dtype!="array_"):
-            tuple_arr.append(io_descriptors_pb2.Value(**{f"{dtype}":item}))
-        elif(dtype=="array_"):
+        if(not dtype):
+            raise ValueError(f'Invalid datatype "{type(item).__name__}" within tuple.')
+        elif(dtype=="timestamp_" and type(item)!=list and type(item)!=datetime.timedelta):
+            if(type(item)==datetime.date):
+                item=datetime.datetime(item.year, item.month, item.day)
+            t=Timestamp()
+            t.FromDatetime(item)
+            tuple_arr.append(io_descriptors_pb2.Value(**{"timestamp_":t}))
+        elif(dtype=="duration_" or type(item)==datetime.timedelta):
+            d=Duration()
+            d.FromTimedelta(item)
+            tuple_arr.append(io_descriptors_pb2.Value(**{"duration_":d}))
+        elif(dtype=="array_" or type(item)==list):
             if(not all(isinstance(x,type(item[0]))for x in item)):
                 val=create_tuple_proto(item)
                 tuple_arr.append(io_descriptors_pb2.Value(tuple_=val))
@@ -49,7 +61,7 @@ def create_tuple_proto(tuple):
                 val=arr_to_proto(item)
                 tuple_arr.append(io_descriptors_pb2.Value(array_=val))
         else:
-            raise ValueError(f'Invalid datatype "{type(item).__name__}" within tuple.')
+            tuple_arr.append(io_descriptors_pb2.Value(**{f"{dtype}":item}))
         
     return io_descriptors_pb2.Tuple(value_=tuple_arr)
 
@@ -60,12 +72,31 @@ def arr_to_proto(arr):
         raise ValueError("Entered tuple, expecting array.")
     
     dtype=is_supported(type(arr[0]))
-    if(dtype and dtype!="array_"):
-        return io_descriptors_pb2.Array(**{"dtype":dtype,f"{dtype}":arr})
-
+    if not dtype:
+        raise ValueError("Dtype is not supported.")
+    if(dtype=="timestamp_" and type(arr[0])!=list):
+        timestamp_arr=[]
+        for dt in arr:
+            if(type(dt)==datetime.date):
+                dt=datetime.datetime(dt.year, dt.month, dt.day)
+            t=Timestamp()
+            t.FromDatetime(dt)
+            timestamp_arr.append(t)
+        return io_descriptors_pb2.NumpyNdarray(dtype="timestamp_",timestamp_=timestamp_arr)
+    elif(dtype=="duration_"):
+        duration_arr=[]
+        for td in arr:
+            d=Duration()
+            d.FromTimedelta(td)
+            duration_arr.append(d)
+        return io_descriptors_pb2.NumpyNdarray(dtype="duration_",duration_=timestamp_arr)
+    elif(dtype!="array_" and type(arr[0])!=list):
+        return io_descriptors_pb2.NumpyNdarray(**{"dtype":dtype,f"{dtype}":arr})
+        
     return_arr=[]
     is_tuple=False
     for i in range(len(arr)):
+        print(arr[i])
         if(not all(isinstance(x,type(arr[i][0]))for x in arr[i])):
             is_tuple=True
             val=create_tuple_proto(arr[i])
@@ -74,9 +105,9 @@ def arr_to_proto(arr):
         return_arr.append(val)
     try:
         if(is_tuple):
-            return_arr=io_descriptors_pb2.Array(dtype="tuple_",tuple_=return_arr)
+            return_arr=io_descriptors_pb2.NumpyNdarray(dtype="tuple_",tuple_=return_arr)
         else:
-            return_arr=io_descriptors_pb2.Array(dtype="array_",array_=return_arr)
+            return_arr=io_descriptors_pb2.NumpyNdarray(dtype="array_",array_=return_arr)
     except:
         raise ValueError("Entered invalid array of inconsistent shape.")
     
@@ -99,7 +130,7 @@ def handle_tuple(proto_tuple):
         elif(tuple_arr[i].WhichOneof("dtype")=="duration_"):
             val=Duration.ToTimedelta(val)
 
-        if(type(val)==io_descriptors_pb2.Array):
+        if(type(val)==io_descriptors_pb2.NumpyNdarray):
             val=proto_to_arr(val)
         elif(type(val)==io_descriptors_pb2.Tuple):
             val=handle_tuple(val)
@@ -120,7 +151,7 @@ def proto_to_arr(proto_arr):
         raise ValueError("Provided array is either empty or invalid")
 
     for i in range(len(return_arr)):
-        if(type(return_arr[i])==io_descriptors_pb2.Array):
+        if(type(return_arr[i])==io_descriptors_pb2.NumpyNdarray):
             return_arr[i]=proto_to_arr(return_arr[i])
         elif(type(return_arr[i])==io_descriptors_pb2.Tuple):
             return_arr[i]=handle_tuple(return_arr[i])
