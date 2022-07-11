27638dc2024776afd8df041f5d609ceffa1e08e2
Sadab Hafiz <sadabhafizny@gmail.com>
2022-07-11 16:40:46 -0400

    Now using async server with graceful shutdown

--- start_grpc_service.py
+++ start_grpc_service.py
@@ -1,6 +1,4 @@
-import os
 import sys
-
 import bentoml
 
 """
@@ -25,7 +23,7 @@ def create_service_proto():
     """
 
     # Create the protobuf file
-    with open("./protos/bentoML.proto", "w") as f:
+    with open("./protos/bentoml_service.proto", "w") as f:
         f.write(
             f"""
 syntax = "proto3";
@@ -41,13 +39,12 @@ message BentoServiceMessage {{
 
 message BentoServiceInput {{
 	BentoServiceMessage input = 1;
-	//string api_name=2;
+	//TODO string api_name=2;
 }}
 
 message BentoServiceOutput {{
 	BentoServiceMessage output= 1;
-	//status
-	//error
+	//TODO status, error
 }}
 
 service BentoML{{
@@ -56,7 +53,6 @@ service BentoML{{
 """
         )
     return api_func
-
 
 def create_server(server_name: str):
     """
@@ -68,16 +64,21 @@ def create_server(server_name: str):
         f.write(
             f"""# Code to create a server using grpc
 import grpc
-import bentoML_pb2
-import bentoML_pb2_grpc
+import bentoml_service_pb2
+import bentoml_service_pb2_grpc
 import bentoml
 from concurrent import futures
 from bentoml.io import NumpyNdarray,Text
 import numpy as np
+import logging
+import asyncio
 
-from grpc_service_util import arr_to_proto,proto_to_arr
+from grpc_service_util import arr_to_proto, proto_to_arr
 
 svc=bentoml.load("{sys.argv[1]}")
+
+for runner in svc.runners:
+    runner.init_local()
 
 for name,api in svc.apis.items():    
     # Create a server and add the lda_model to the server
@@ -90,7 +91,7 @@ type_dict={{
     "array" : NumpyNdarray 
 }}
 
-class BentoMLServicer(bentoML_pb2_grpc.BentoMLServicer):
+class BentoMLServicer(bentoml_service_pb2_grpc.BentoMLServicer):
     def {api_func.__name__}(self, request, context):
         input= request.input
 
@@ -110,32 +111,77 @@ class BentoMLServicer(bentoML_pb2_grpc.BentoMLServicer):
         output=svc_func(input)
 
         if(type(output_type)==Text):
-            output=bentoML_pb2.BentoServiceMessage(text=output)
+            output=bentoml_service_pb2.BentoServiceMessage(text=output)
         elif(type(output_type)==NumpyNdarray):
             out=arr_to_proto(output)
-            output=bentoML_pb2.BentoServiceMessage(array=out)
-        return bentoML_pb2.BentoServiceOutput(output=output)
+            output=bentoml_service_pb2.BentoServiceMessage(array=out)
+        return bentoml_service_pb2.BentoServiceOutput(output=output)
 
-# Create a server and add the lda_model to the server
-server=grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-bentoML_pb2_grpc.add_BentoMLServicer_to_server(BentoMLServicer(),server)
-server.add_insecure_port('[::]:8000')
+_cleanup_coroutines = []
+async def serve() -> None:
+    server = grpc.aio.server()
+    bentoml_service_pb2_grpc.add_BentoMLServicer_to_server(BentoMLServicer(),server)
+    listen_addr = '[::]:50051'
+    server.add_insecure_port(listen_addr)
+    logging.info("Starting server on %s", listen_addr)
+    await server.start()
+    async def server_graceful_shutdown():
+        logging.info("Starting graceful shutdown...")
+        # Shuts down the server with 5 seconds of grace period. During the
+        # grace period, the server won't accept new connections and allow
+        # existing RPCs to continue within the grace period.
+        await server.stop(5)
+    _cleanup_coroutines.append(server_graceful_shutdown())
+    await server.wait_for_termination()
 
-# Start server and keep the server running until terminated\n
-server.start()
-print("Starting server...")
-print("Listening on port 8000...")
-server.wait_for_termination()
+logging.basicConfig(level=logging.INFO)
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(serve())
+finally:
+    loop.run_until_complete(*_cleanup_coroutines)
+    loop.close()
 """
         )
 
-
 create_service_proto()
-os.system(
-    "python -m grpc_tools.protoc -I./protos --python_out=. protos/io_descriptors.proto"
+
+# import pkg_resources
+# from grpc_tools import protoc
+# proto_include = pkg_resources.resource_filename('grpc_tools', '_proto')
+# curr_dir = os.getcwd()
+# protoc.main([
+#     'grpc_tools.protoc',
+#     f'-I{proto_include}',
+#     '--proto_path=./protos',
+#     '--python_out=./',
+#     'protos/io_descriptors.proto'
+# ])
+
+# protoc.main([
+#     'grpc_tools.protoc',
+#     f'-I{proto_include}',
+#     '--proto_path=./protos',
+#     '--python_out=./',
+#     '--grpc_python_out=./',
+#     'protos/bentoml_service.proto'
+# ])
+
+import subprocess
+
+subprocess.run(
+    "python -m grpc_tools.protoc -I./protos --python_out=. protos/io_descriptors.proto",
+    shell = True
 )
-os.system(
-    "python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. protos/bentoML.proto"
+
+subprocess.run(
+    "python -m grpc_tools.protoc -I./protos --python_out=. --grpc_python_out=. protos/bentoml_service.proto",
+    shell = True
 )
+
 create_server("server.py")
-os.system("python server.py")
+
+subprocess.run(
+    "python server.py",
+    shell=True
+)
